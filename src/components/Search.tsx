926e54ca4747b515f3c09493a52fd9bc528edd61
Halyna Stehnii <galyna.stegniy@gmail.com>
2025-02-02 23:17:17 +0100

    feat: create Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,25 @@
+import { Component, ChangeEvent } from 'react';
+
+interface SearchProps {
+  searchTerm: string;
+  onSearchChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onSearch: () => void;
+}
+
+class Search extends Component<SearchProps> {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.props.searchTerm}
+          onChange={this.props.onSearchChange}
+          placeholder="Find StarWars characters"
+        />
+        <button onClick={this.props.onSearch}>Search</button>
+      </div>
+    );
+  }
+}
+
+export default Search;
