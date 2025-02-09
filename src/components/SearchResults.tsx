b570acebf1e63803033f60722c5d86b0c3fbdb30
Halyna Stehnii <galyna.stegniy@gmail.com>
2025-02-09 16:38:32 +0100

    feat: SearchResults change to functional components

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,27 +1,24 @@
-import React from 'react';
 import { SearchResult } from '../App';
 
 interface SearchResultsProps {
   searchResults: SearchResult;
 }
 
-class SearchResults extends React.Component<SearchResultsProps> {
-  render() {
-    return (
-      <div>
-        <h2>Search Results</h2>
-        <ul className="search-results">
-          {this.props.searchResults.results.map((result, index) => (
-            <li key={index}>
-              <div>Name: {result.name}</div>
-              <div>Gender: {result.gender}</div>
-              <div>Birth year: {result.birth_year}</div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const SearchResults = ({ searchResults }: SearchResultsProps) => {
+  return (
+    <div>
+      <h2>Search Results</h2>
+      <ul className="search-results">
+        {searchResults.results.map((result, index) => (
+          <li key={index}>
+            <div>Name: {result.name}</div>
+            <div>Gender: {result.gender}</div>
+            <div>Birth year: {result.birth_year}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default SearchResults;
