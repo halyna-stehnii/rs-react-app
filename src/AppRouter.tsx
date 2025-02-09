86f44a18788856eee8517e85fd3caba6c3aff634
Halyna Stehnii <galyna.stegniy@gmail.com>
2025-02-09 23:00:37 +0100

    fix: ErrorBoundary

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,17 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import CharacterDetails from './components/CharacterDetails';
 import NotFound from './components/NotFound';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="character/:id" element={<CharacterDetails />} />
-        </Route>
-        <Route path="not-found" element={<NotFound />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="character/:id" element={<CharacterDetails />} />
+          </Route>
+          <Route path="not-found" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 };
