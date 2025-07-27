6b2400c32cdf908175fdc0fc8d607fbd3b2a2282
Halyna Stehnii <galyna.stegniy@gmail.com>
2025-07-27 23:01:29 +0200

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
-import ErrorBoundary from './components/ErrorBoundary.tsx';
+import AppRouter from './AppRouter.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error('Failed to find root element');
 }
 
-createRoot(rootElement).render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>
-);
+createRoot(rootElement).render(<AppRouter />);
